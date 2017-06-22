8c1767aebb14ac6b80f57c7d9bce953be53d3fb9
Cameron Bass <cam@collectiveidea.com>
2017-06-22 13:28:07 -0400

    add new line to prod file

--- config/prod.exs
+++ config/prod.exs
@@ -1,5 +1,11 @@
 use Mix.Config
 
+config :hello_phoenix, HelloPhoenix.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
+
 # For production, we configure the host to read the PORT
 # from the system environment. Therefore, you will need
 # to set PORT=80 before running your server.
@@ -13,8 +19,9 @@ use Mix.Config
 # which you typically run after static files are built.
 config :shop_time, ShopTime.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [host: "ci-shoppers.herokuapp.com", port: 80],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -24,13 +31,13 @@ config :logger, level: :info
 # To get SSL working, you will need to add the `https` key
 # to the previous section and set your `:url` port to 443:
 #
-#     config :shop_time, ShopTime.Endpoint,
-#       ...
-#       url: [host: "example.com", port: 443],
-#       https: [port: 443,
-#               keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-#               certfile: System.get_env("SOME_APP_SSL_CERT_PATH")]
-#
+    #  config :shop_time, ShopTime.Endpoint,
+    #   ...
+    #  url: [host: "example.com", port: 443],
+    #   https: [port: 443,
+    #           keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
+    #           certfile: System.get_env("SOME_APP_SSL_CERT_PATH")]
+
 # Where those two env variables return an absolute path to
 # the key and cert in disk or a relative path inside priv,
 # for example "priv/ssl/server.key".
