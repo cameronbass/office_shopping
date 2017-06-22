2bac9956fd4fd6ddc44d2adc652ce454fa221219
Cameron Bass <cam@collectiveidea.com>
2017-06-21 23:12:13 -0400

    chat is working

--- lib/shop_time.ex
+++ lib/shop_time.ex
@@ -14,6 +14,7 @@ defmodule ShopTime do
       supervisor(ShopTime.Endpoint, []),
       # Start your own worker by calling: ShopTime.Worker.start_link(arg1, arg2, arg3)
       # worker(ShopTime.Worker, [arg1, arg2, arg3]),
+      supervisor(ShopTime.Presence, []),
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
